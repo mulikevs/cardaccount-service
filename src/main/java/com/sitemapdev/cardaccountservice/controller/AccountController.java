8063e32b5a6c9e0e6f987b29d1bde63ab272e052
Kelvin M. Mwau [ICT] <mulikevs@gmail.com>
2024-01-21 19:03:00 +0300

    added logging

--- src/main/java/com/sitemapdev/cardaccountservice/controller/AccountController.java
+++ src/main/java/com/sitemapdev/cardaccountservice/controller/AccountController.java
@@ -1,19 +1,17 @@
 package com.sitemapdev.cardaccountservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.sitemapdev.cardaccountservice.entity.Account;
 import com.sitemapdev.cardaccountservice.entity.Card;
 import com.sitemapdev.cardaccountservice.service.AccountService;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
@@ -23,38 +21,55 @@ public class AccountController {
 
     @GetMapping
     public ResponseEntity<List<Account>> getAllAccounts() {
+        log.info("Fetching all accounts");
         List<Account> accounts = accountService.getAllAccounts();
+        log.debug("Fetched {} accounts", accounts.size());
         return new ResponseEntity<>(accounts, HttpStatus.OK);
     }
 
     @GetMapping("/{accountId}")
     public ResponseEntity<Account> getAccountById(@PathVariable Long accountId) {
+        log.info("Fetching account by ID: {}", accountId);
         Account account = accountService.getAccountById(accountId);
-        return new ResponseEntity<>(account, HttpStatus.OK);
+
+        if (account != null) {
+            log.debug("Fetched account: {}", account);
+            return new ResponseEntity<>(account, HttpStatus.OK);
+        } else {
+            log.warn("Account with ID {} not found", accountId);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping
     public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        log.info("Creating account: {}", account);
         Account createdAccount = accountService.createAccount(account);
+        log.debug("Created account: {}", createdAccount);
         return new ResponseEntity<>(createdAccount, HttpStatus.CREATED);
     }
 
     @PutMapping("/{accountId}")
     public ResponseEntity<Account> updateAccount(@PathVariable Long accountId, @RequestBody Account account) {
+        log.info("Updating account with ID {}: {}", accountId, account);
         Account updatedAccount = accountService.updateAccount(accountId, account);
+        log.debug("Updated account: {}", updatedAccount);
         return new ResponseEntity<>(updatedAccount, HttpStatus.OK);
     }
 
     @DeleteMapping("/{accountId}")
     public ResponseEntity<Void> deleteAccount(@PathVariable Long accountId) {
+        log.info("Deleting account with ID: {}", accountId);
         accountService.deleteAccount(accountId);
+        log.debug("Account with ID {} deleted", accountId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/{accountId}/cards")
     public ResponseEntity<List<Card>> getCardsByAccountId(@PathVariable Long accountId) {
+        log.info("Fetching cards for account with ID: {}", accountId);
         List<Card> cards = accountService.getCardsByAccountId(accountId);
+        log.debug("Fetched {} cards for account with ID {}", cards.size(), accountId);
         return new ResponseEntity<>(cards, HttpStatus.OK);
     }
 }
-
--- src/main/java/com/sitemapdev/cardaccountservice/controller/CardController.java
+++ src/main/java/com/sitemapdev/cardaccountservice/controller/CardController.java
@@ -1,25 +1,16 @@
 package com.sitemapdev.cardaccountservice.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.sitemapdev.cardaccountservice.entity.Card;
-import com.sitemapdev.cardaccountservice.service.CardService;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.sitemapdev.cardaccountservice.entity.Card;
+import com.sitemapdev.cardaccountservice.service.CardService;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/cards")
 public class CardController {
@@ -29,25 +20,33 @@ public class CardController {
 
     @GetMapping("/by-account/{accountId}")
     public ResponseEntity<List<Card>> getCardsByAccountId(@PathVariable Long accountId) {
+        log.info("Fetching cards for account with ID: {}", accountId);
         List<Card> cards = cardService.getCardsByAccountId(accountId);
+        log.debug("Fetched {} cards for account with ID {}", cards.size(), accountId);
         return new ResponseEntity<>(cards, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Card> createCard(@RequestBody Card card) {
+        log.info("Creating card: {}", card);
         Card createdCard = cardService.createCard(card);
+        log.debug("Created card: {}", createdCard);
         return new ResponseEntity<>(createdCard, HttpStatus.CREATED);
     }
 
     @PutMapping("/{cardId}")
     public ResponseEntity<Card> updateCard(@PathVariable Long cardId, @RequestBody Card card) {
+        log.info("Updating card with ID {}: {}", cardId, card);
         Card updatedCard = cardService.updateCard(cardId, card);
+        log.debug("Updated card: {}", updatedCard);
         return new ResponseEntity<>(updatedCard, HttpStatus.OK);
     }
 
     @DeleteMapping("/{cardId}")
     public ResponseEntity<Void> deleteCard(@PathVariable Long cardId) {
+        log.info("Deleting card with ID: {}", cardId);
         cardService.deleteCard(cardId);
+        log.debug("Card with ID {} deleted", cardId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
